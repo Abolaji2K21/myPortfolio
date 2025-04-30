7d78c20512605963f77f64df0378d42f1a59c27c
Abolaji2K21 <abisoyeabolaji2k21@gmail.com>
2025-04-30 11:47:08 +0100

    update Aboutme component

--- src/pages/about/Info.jsx
+++ src/pages/about/Info.jsx
@@ -6,12 +6,12 @@ const Info = () => {
             <div className="about__box">
                 <i className="bx bx-award about__icon"></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">1 Year of Hands-on Training</span>
+                <span className="about__subtitle">2+ Year of Hands-on Training</span>
             </div>
             <div className="about__box">
                 <i className="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Projects</h3>
-                <span className="about__subtitle">10+ Completed Projects</span>
+                <span className="about__subtitle">15+ Completed Projects</span>
             </div>
             <div className="about__box">
                 <i className="bx bx-support about__icon"></i>
